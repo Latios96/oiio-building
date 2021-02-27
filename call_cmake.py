d1cf642cd96690d58784d4692bf93bf5bfe397d9
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-02-27 23:24:23 +0100

    add missing

---
+++ call_cmake.py
@@ -0,0 +1,23 @@
+import configparser
+import subprocess
+import json
+
+if __name__ == '__main__':
+    with open('conanbuildinfo.json', 'r') as f:
+        data = json.load(f)
+    dependencies = data['dependencies']
+    openexr_dependency = [x for x in dependencies if x['name'] == 'openexr'][0]
+    openexr_root = openexr_dependency['rootpath']
+    cmake_command = ["cmake",
+                     "-DUSE_PYTHON=1",
+                     "-DPYTHON_VERSION=3.8",
+                     "-DUSE_EXTERNAL_PUGIXML=1",
+                     "-DOIIO_BUILD_TESTS=0",
+                     "-DOIIO_BUILD_TOOLS=0",
+                     "-DBUILD_SHARED_LIBS=0",
+                     "-DSTOP_ON_WARNING=OFF",
+                     "-DENABLE_WebP=0",
+                     "-DENABLE_FFmpeg=0",
+                     "-DOPENEXR_ROOT={}".format(openexr_root),
+                     "../oiio"]
+    subprocess.call(cmake_command)
