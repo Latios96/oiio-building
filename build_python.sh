33d6c724622d5371fbe5e97507ebd0a8cba913bd
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-02-27 23:21:19 +0100

    fixing build

--- build_python.sh
+++ build_python.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 function buildCpp {
 	conan install .. --build missing
-	cmake \
-		-DUSE_PYTHON=1 \
-		-DPYTHON_VERSION=3.8 \
-		-DUSE_EXTERNAL_PUGIXML=1 \
-		-DOIIO_BUILD_TESTS=0 \
-		-DOIIO_BUILD_TOOLS=0 \
-		-DBUILD_SHARED_LIBS=0 \
-		-DSTOP_ON_WARNING=OFF \
-		-DENABLE_WebP=0 \
-		-DENABLE_FFmpeg=0 \
-		../oiio
-	make -j 4	
+	python3 ../call_cmake.py
+	make -j 16	
 }
 
 function buildWheel {
