a1fb6ec7f6276d75fea7489626088485be8f2ec3
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-02-27 21:54:23 +0100

    pin version to tag

--- run_linux.sh
+++ run_linux.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 git clone https://github.com/OpenImageIO/oiio.git
+cd oiio
+git checkout Release-2.2.11.1
+cd ..
 ROOT=$PWD
 oiio/src/build-scripts/build_pybind11.bash
 export pybind11_ROOT=/$PWD/ext/dist
