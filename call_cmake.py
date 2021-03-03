9218534540530809c703230d10464e1b6d967898
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-03-03 19:41:42 +0100

    deps paths

--- call_cmake.py
+++ call_cmake.py
@@ -1,4 +1,3 @@
-import configparser
 import subprocess
 import json
 
@@ -6,8 +5,10 @@ if __name__ == '__main__':
     with open('conanbuildinfo.json', 'r') as f:
         data = json.load(f)
     dependencies = data['dependencies']
-    openexr_dependency = [x for x in dependencies if x['name'] == 'openexr'][0]
-    openexr_root = openexr_dependency['rootpath']
+    deps = []
+    for dep in dependencies:
+        deps.append("-D{}_ROOT={}".format(dep['name'].upper(), dep['rootpath']))
+
     cmake_command = ["cmake",
                      "-DUSE_PYTHON=1",
                      "-DPYTHON_VERSION=3.8",
@@ -18,6 +19,7 @@ if __name__ == '__main__':
                      "-DSTOP_ON_WARNING=OFF",
                      "-DENABLE_WebP=0",
                      "-DENABLE_FFmpeg=0",
-                     "-DOPENEXR_ROOT={}".format(openexr_root),
+                     *deps,
                      "../oiio"]
+    print(cmake_command)
     subprocess.call(cmake_command)
