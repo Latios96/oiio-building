c556d9e2ed1e98f13d80838d1154607a236c77f0
Jan Honsbrok <jan.honsbrok@gmail.com>
2021-03-03 21:23:38 +0100

    try other deps script

--- run_linux.sh
+++ run_linux.sh
@@ -3,9 +3,10 @@ git clone https://github.com/OpenImageIO/oiio.git
 cd oiio
 git checkout Release-2.2.11.1
 cd ..
-ROOT=$PWD
-oiio/src/build-scripts/build_pybind11.bash
-export pybind11_ROOT=/$PWD/ext/dist
-cd $ROOT
-echo $PWD
+#ROOT=$PWD
+#oiio/src/build-scripts/build_pybind11.bash
+#export pybind11_ROOT=/$PWD/ext/dist
+#cd $ROOT
+#echo $PWD
+./oiio/src/build-scripts/gh-installdeps.bash
 ./build_python.sh
